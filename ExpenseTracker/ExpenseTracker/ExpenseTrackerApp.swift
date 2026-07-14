19e9080cbbf8ef10eb01228463578da67ddd2c35
Andrew <andrew-282011@mail.ru>
2026-07-14 19:49:14 +0300

    Create a tracker

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Andrew on 14.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
